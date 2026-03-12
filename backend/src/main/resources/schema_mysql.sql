9e9f6ce8c6d4826905bf6347e701b39d7ac985c1
gyaneshwar01 <gyaneshwar585@gmail.com>
2026-03-12 19:00:03 +0545

    ready to deploy

--- backend/src/main/resources/schema_mysql.sql
+++ backend/src/main/resources/schema_mysql.sql
@@ -1,5 +1,3 @@
-use mmms_db;
-
 CREATE TABLE IF NOT EXISTS app_users
 (
     uid       INT AUTO_INCREMENT PRIMARY KEY,
@@ -10,104 +8,110 @@ CREATE TABLE IF NOT EXISTS app_users
     password VARCHAR(100)
 );
 
-CREATE TABLE IF NOT EXISTS committees (
-                                          committee_id INT AUTO_INCREMENT PRIMARY KEY,
-                                          uuid VARCHAR(36) NOT NULL UNIQUE,
-                                          committee_name VARCHAR(255) NOT NULL,
-                                          committee_description TEXT NOT NULL,
-                                          created_by INT NOT NULL,
-                                          created_date DATE NOT NULL,
-                                          modified_by VARCHAR(255) NOT NULL,
-                                          modified_date DATE NOT NULL,
-                                          status VARCHAR(255) NOT NULL,
-                                          max_no_of_meetings INT,
-                                          FOREIGN KEY (created_by) REFERENCES app_users(uid)
+CREATE TABLE IF NOT EXISTS members (
+    member_id INT AUTO_INCREMENT PRIMARY KEY,
+    member_uuid VARCHAR(36) NOT NULL UNIQUE,
+
+    member_first_name VARCHAR(255) NOT NULL,
+    member_last_name VARCHAR(255) NOT NULL,
+
+    member_post VARCHAR(255),
+    member_title VARCHAR(255) NOT NULL,
+
+    member_created_by VARCHAR(255) NOT NULL,
+    member_created_date DATE NOT NULL,
+    member_modified_by VARCHAR(255) NOT NULL,
+    member_modified_date DATE NOT NULL
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-
-CREATE TABLE IF NOT EXISTS members (
-                                       member_id INT AUTO_INCREMENT PRIMARY KEY,
-                                       uuid VARCHAR(36) NOT NULL UNIQUE,
-
-                                       first_name VARCHAR(255) NOT NULL,
-                                       last_name VARCHAR(255) NOT NULL,
-
-                                       first_name_nepali VARCHAR(255) NULL,
-                                       last_name_nepali VARCHAR(255) NULL,
-
-                                       institution VARCHAR(255),
-                                       post VARCHAR(255),
-                                       qualification VARCHAR(255),
-                                       email VARCHAR(255),
-
-                                       created_by VARCHAR(255) NOT NULL,
-                                       created_date DATE NOT NULL,
-                                       modified_by VARCHAR(255) NOT NULL,
-                                       modified_date DATE NOT NULL
+CREATE TABLE IF NOT EXISTS committees (
+    committee_id INT AUTO_INCREMENT PRIMARY KEY,
+    committee_coordinator_id INT NOT NULL,
+    committee_description TEXT,
+    committee_uuid VARCHAR(36) NOT NULL UNIQUE,
+    committee_name VARCHAR(255) NOT NULL,
+    committee_created_by VARCHAR(255) NOT NULL,
+    committee_created_date DATE NOT NULL,
+    committee_modified_by VARCHAR(255) NOT NULL,
+    committee_modified_date DATE NOT NULL,
+    committee_status VARCHAR(255) NOT NULL,
+    committee_minute_language VARCHAR(255) NOT NULL,
+    committee_max_no_of_meetings INT,
+    FOREIGN KEY (committee_coordinator_id) REFERENCES members(member_id)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS committee_memberships (
-                                                     committee_id INT NOT NULL,
-                                                     uuid VARCHAR(36) NOT NULL UNIQUE,
-                                                     member_id INT NOT NULL,
-                                                     role VARCHAR(255) NOT NULL,
-                                                     PRIMARY KEY (committee_id, member_id),
-                                                     FOREIGN KEY (committee_id) REFERENCES committees(committee_id),
-                                                     FOREIGN KEY (member_id) REFERENCES members(member_id)
+    committee_id INT NOT NULL,
+    uuid VARCHAR(36) NOT NULL UNIQUE,
+    member_id INT NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    display_order INT NOT NULL,
+    PRIMARY KEY (committee_id, member_id),
+    FOREIGN KEY (committee_id) REFERENCES committees(committee_id),
+    FOREIGN KEY (member_id) REFERENCES members(member_id)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS meetings (
-                                        meeting_id INT AUTO_INCREMENT PRIMARY KEY,
-                                        uuid VARCHAR(36) NOT NULL UNIQUE,
+    meeting_id INT AUTO_INCREMENT PRIMARY KEY,
+    uuid VARCHAR(36) NOT NULL UNIQUE,
 
-                                        meeting_title VARCHAR(255) NOT NULL,
-                                        meeting_description TEXT,
-                                        meeting_held_date DATE NOT NULL,
-                                        meeting_held_time TIME NOT NULL,  -- Added this line for LocalTime
-                                        meeting_held_place VARCHAR(255) NOT NULL,
-                                        created_by VARCHAR(255) NOT NULL,
-                                        updated_by VARCHAR(255) NOT NULL,
-                                        created_date DATE NOT NULL,
-                                        updated_date DATE NOT NULL,
+    meeting_title VARCHAR(255) NOT NULL,
+    meeting_held_date DATE NOT NULL,
+    meeting_held_time TIME NOT NULL,
+    meeting_held_place VARCHAR(255) NOT NULL,
+    created_by VARCHAR(255) NOT NULL,
+    updated_by VARCHAR(255) NOT NULL,
+    created_date DATE NOT NULL,
+    updated_date DATE NOT NULL,
 
-                                        committee_id INT NOT NULL,
-                                        FOREIGN KEY (committee_id) REFERENCES committees(committee_id)
+    committee_id INT NOT NULL,
+    FOREIGN KEY (committee_id) REFERENCES committees(committee_id)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
 
 CREATE TABLE IF NOT EXISTS meeting_attendees (
-                                                 member_id INT NOT NULL,
-                                                 meeting_id INT NOT NULL,
+    member_id INT NOT NULL,
+    meeting_id INT NOT NULL,
 
-                                                 PRIMARY KEY (member_id, meeting_id),
-                                                 FOREIGN KEY (member_id) REFERENCES members(member_id),
-                                                 FOREIGN KEY (meeting_id) REFERENCES meetings(meeting_id)
+    PRIMARY KEY (member_id, meeting_id),
+    FOREIGN KEY (member_id) REFERENCES members(member_id),
+    FOREIGN KEY (meeting_id) REFERENCES meetings(meeting_id)
 );
 
-CREATE TABLE IF NOT EXISTS meeting_invitees(
-                                               member_id INT NOT NULL,
-                                               meeting_id INT NOT NULL,
+CREATE TABLE IF NOT EXISTS meeting_invitees (
+    member_id INT NOT NULL,
+    meeting_id INT NOT NULL,
 
-                                               PRIMARY KEY (member_id, meeting_id),
-                                               FOREIGN KEY (member_id) REFERENCES members(member_id),
-                                               FOREIGN KEY (meeting_id) REFERENCES meetings(meeting_id)
-);
-
-CREATE TABLE IF NOT EXISTS decisions (
-                                         decision_id INT AUTO_INCREMENT PRIMARY KEY,
-                                         uuid VARCHAR(36) NOT NULL UNIQUE,
-
-                                         meeting_id INT NOT NULL,
-                                         decision TEXT,
-                                         FOREIGN KEY (meeting_id) REFERENCES meetings(meeting_id)
+    PRIMARY KEY (member_id, meeting_id),
+    FOREIGN KEY (member_id) REFERENCES members(member_id),
+    FOREIGN KEY (meeting_id) REFERENCES meetings(meeting_id)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+CREATE TABLE IF NOT EXISTS decisions (
+    decision_id INT AUTO_INCREMENT PRIMARY KEY,
+    uuid VARCHAR(36) NOT NULL UNIQUE,
+
+    meeting_id INT NOT NULL,
+    decision TEXT,
+
+    decision_created_by VARCHAR(255) NOT NULL,
+    decision_created_date DATE NOT NULL,
+    decision_modified_by VARCHAR(255) NOT NULL,
+    decision_modified_date DATE NOT NULL,
+
+    FOREIGN KEY (meeting_id) REFERENCES meetings(meeting_id)
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS agendas (
-                                       agenda_id INT AUTO_INCREMENT PRIMARY KEY,
-                                       uuid VARCHAR(36) NOT NULL UNIQUE,
+    agenda_id INT AUTO_INCREMENT PRIMARY KEY,
+    uuid VARCHAR(36) NOT NULL UNIQUE,
 
-                                       meeting_id INT NOT NULL,
-                                       agenda TEXT,
-                                       FOREIGN KEY (meeting_id) REFERENCES meetings(meeting_id)
+    meeting_id INT NOT NULL,
+    agenda TEXT,
+
+    agenda_created_by VARCHAR(255) NOT NULL,
+    agenda_created_date DATE NOT NULL,
+    agenda_modified_by VARCHAR(255) NOT NULL,
+    agenda_modified_date DATE NOT NULL,
+
+    FOREIGN KEY (meeting_id) REFERENCES meetings(meeting_id)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
