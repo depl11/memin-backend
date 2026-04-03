a786c8d3062bbc75f20c74de73e64a8a9dd36176
gyaneshwar01 <gyaneshwar585@gmail.com>
2026-04-03 13:28:23 +0545

    alter table add column

--- backend/src/main/resources/schema_mysql.sql
+++ backend/src/main/resources/schema_mysql.sql
@@ -25,6 +25,9 @@ CREATE TABLE IF NOT EXISTS members (
     member_modified_date DATE NOT NULL
     ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+ALTER TABLE members
+    ADD COLUMN member_institution VARCHAR(255);
+
 CREATE TABLE IF NOT EXISTS committees (
                                           committee_id INT AUTO_INCREMENT PRIMARY KEY,
                                           committee_coordinator_id INT NOT NULL,
